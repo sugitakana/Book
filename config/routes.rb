a0400c7c886cb5aa1fa66c7c0531d37f77fb2b81
sugitakana <126451068+sugitakana@users.noreply.github.com>
2023-07-03 11:10:58 +0000

    [Add]top画面

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   registrations: "public/registrations",
   sessions: 'public/sessions'
   }
+  scope module: :public do
+    root to: "homes#top"
+  end
   
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
   sessions: "admin/sessions"
