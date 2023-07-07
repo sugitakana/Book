02c5909ec602ae81a2f8b2ed05a8ce57c056817c
sugitakana <126451068+sugitakana@users.noreply.github.com>
2023-07-07 11:02:47 +0000

    Add]プロフ

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,13 +1,14 @@
 class Public::UsersController < ApplicationController
   def index
   end
-  
+
   def show
+    @user = User.find(params[:id])
   end
-  
+
   def edit
   end
-  
+
   def update
   end
 end
